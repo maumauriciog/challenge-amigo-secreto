a2de679ba90588f78a6bc903035b1143577e6134
Task View <maumauriciog@hotmail.com>
2025-02-21 08:06:41 -0300

    Adicionando a Função adicionaAmigos

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let nomeAmigo = document.getElementById('amigo');
+let arrayNomeAmigo = [];
+let listaAmigos = document.getElementById('listaAmigos');
+
+function adicionarAmigo(){
+    console.log(nomeAmigo.value);
+    arrayNomeAmigo.push(' ' + nomeAmigo.value);
+
+    listaAmigos.innerHTML = arrayNomeAmigo;
+    console.log(arrayNomeAmigo);
+
+    nomeAmigo.value = '';
+}
