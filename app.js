19eb7c5e8ef18333c0322ffc5369ec931f385e45
Task View <maumauriciog@hotmail.com>
2025-02-24 09:50:20 -0300

    alterações arquivo v1.1

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let nomeAmigo = document.getElementById('amigo');
-let arrayNomeAmigo = [];
 let listaAmigos = document.getElementById('listaAmigos');
+let resultado = document.getElementById('resultado');
+let arrayAmigos = [];
+let li = '';
+let ul = '<ul>';
+let indice = 0;
+indice = parseInt(indice);
 
-function adicionarAmigo(){
-    console.log(nomeAmigo.value);
-    arrayNomeAmigo.push(' ' + nomeAmigo.value);
+function adicionarAmigo() {
+    if (nomeAmigo.value) {
+        arrayAmigos.push(nomeAmigo.value);
+        li = `<li>${arrayAmigos[indice]}</li>`;
+        ul += li;
+        listaAmigos.innerHTML = ul;
+        nomeAmigo.value = '';
+        resultado.innerHTML = '';
+        indice += 1;
+    } else {
+        listaAmigos.innerHTML = ul;
+        resultado.innerHTML = 'precisa inserir um amigo...';
+    }
+}
 
-    listaAmigos.innerHTML = arrayNomeAmigo;
-    console.log(arrayNomeAmigo);
+function sortearAmigo() {
+    if (arrayAmigos.length) {
+        listaAmigos.innerHTML = '';
 
-    nomeAmigo.value = '';
+
+        
+        resultado.innerHTML = arrayAmigos.length;
+        arrayAmigos = [];
+        ul = '';
+    } else {
+        resultado.innerHTML = 'não existem amigos para o sorteio...';
+    }
 }
